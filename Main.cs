9449678eaf5c04cacbd409d54e23d7609853ae9e
AshuraSchoolAccount <kdlall@students.wcpss.net>
2026-08-26 07:30:00 -0400

    Main code
    This is the main code for Version ONE. You can run this code at a C# compiler online, or you may compile the code yourself. (I reccomend using a online compiler due to this version not needed any libraries and the solution not being setup.

---
+++ Main.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace Main 
+{ 
+    public class Program 
+    { 
+        public static string commands = "HELP, EXIT"; 
+        public static string fixedInput = ""; 
+        
+        static void LoadTerminal() 
+        { 
+            try 
+            { 
+                Console.Write("Enter command: ");
+                string userInput = Console.ReadLine(); 
+                fixedInput = userInput != null ? userInput.ToUpper().Trim() : ""; 
+            } 
+            catch (Exception e) 
+            { 
+                Console.WriteLine("Code could not run due to: " + e.Message); 
+            } 
+        } 
+        
+        static void StartTerminal() 
+        { 
+            switch (fixedInput) 
+            { 
+                case "HELP": 
+                    Console.WriteLine("Commands that you can run are: " + commands); 
+                    break; 
+                case "EXIT":
+                    Console.WriteLine("Exiting terminal...");
+                    Environment.Exit(0);
+                    break;
+
+                case "FETCH IP":
+                    Console.WriteLine();
+                    break;
+                
+                default: 
+                    Console.WriteLine("Command is not a valid command. Enter the command help for a list of the current commands"); 
+                    break; 
+            } 
+        } 
+        
+        public static void Main(string[] args) 
+        { 
+            Console.WriteLine("Terminal Loaded");
+            
+            while (true) 
+            {
+                LoadTerminal(); 
+                StartTerminal();
+                Console.WriteLine();
+            } 
+        } 
+    } 
+}
